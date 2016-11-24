44421311922de21348667ec8b212466ca965ee07
chrysler5798 <chrysler5798@hotmail.fr>
2016-11-24 23:24:15 +0100

    +Creation du splash Class + Layout +Ajout de 3 animations (Fade pour le texte et slide+zoom pour logo) +Modification de ic_launcher par le logo de l'app+ +Modification de colors et string +Edit dans le manifest

---
+++ app/src/main/java/com/byethost6/thefreelancer/howmuchtime/SplashActivity.java
@@ -0,0 +1,77 @@
+package com.byethost6.thefreelancer.howmuchtime;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static android.R.attr.animation;
+import static android.R.transition.fade;
+import static java.lang.Thread.sleep;
+
+/**
+ * Created by Chrysler on 11/24/2016.
+ * <p>
+ * howmuchtime
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    Animation zoomout, moveup, fade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        zoomout = AnimationUtils.loadAnimation(this, R.anim.zoom_out);
+        moveup = AnimationUtils.loadAnimation(this, R.anim.translate);
+        fade = AnimationUtils.loadAnimation(this, R.anim.alpha);
+
+        final TextView txt1 = (TextView) findViewById(R.id.textHowmuch);
+        final TextView txt2 = (TextView) findViewById(R.id.textTime);
+        final ImageView logo = (ImageView) findViewById(R.id.logoSplash);
+
+        logo.setAnimation(moveup);
+        txt1.setAnimation(fade);
+
+        moveup.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                logo.startAnimation(zoomout);
+                txt2.setAlpha(1);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        fade.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent myIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
